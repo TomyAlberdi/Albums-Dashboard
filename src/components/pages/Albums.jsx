03df3cd0b9cb766c0613b691a89ee716bdbbce5b
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-04-01 01:30:05 -0300

    Implemented Article Grid template

--- src/components/pages/Albums.jsx
+++ src/components/pages/Albums.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from 'react'
+import Album from '../utils/Album'
 
 const Albums = ({BASE_URL,API_KEY}) => {
     
     const METHOD = "?method=user.gettopalbums&user=tomyalberdi"
     const PERIOD = "period=1month"
 
-    const [Data, setData] = useState({})
+    const [Data, setData] = useState()
     const [LoadingData, setLoadingData] = useState(true)
     useEffect(() => {
         const fetchData = async () => {
             fetch(`${BASE_URL}/${METHOD}&${API_KEY}&format=json&${PERIOD}&limit=20`)
             .then(res => res.json())
             .then(data => {
-                setData(data)
+                setData(data.topalbums.album)
                 setLoadingData(false)
             })
         }
@@ -26,10 +27,12 @@ const Albums = ({BASE_URL,API_KEY}) => {
             </section>
             <section className="content">
                 {
-                    LoadingData ?
-                        <h2 className="loadingMsg">Loading Albums</h2>
-                    :
-                        <h2>Listo</h2>
+                    LoadingData ? <h2 className="loadingMsg">Loading Albums</h2> :
+                    Data.length > 0 ?
+                        Data.map((e,index) => {
+                            return <Album key={index} data={e} />
+                        })
+                    : <h2 className="loadingMsg">No Albums to Load</h2>
                 }
             </section>
         </section>
---
+++ src/components/utils/Album.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Album = ({data}) => {
+    return (
+        <article className="Album">
+            <h2>album</h2>
+        </article>
+    )
+}
+
+export default Album
