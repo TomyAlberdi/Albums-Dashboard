044694c4481c20da2fb416cb16810db7b8f63ce8
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-04-01 01:03:12 -0300

    remade navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,15 @@ const Navbar = () => {
     return (
         <header className='Navbar'>
             <div className="links">
-                <Link to={'/Albums-Dashboard/'}>Albums</Link>
-                <Link to={'/Albums-Dashboard/Artists'}>Artists</Link>
-                <Link to={'/Albums-Dashboard/Tracks'}>Tracks</Link>
+                <div className="navLinks">
+                    <Link to={'/Albums-Dashboard/'}>Albums</Link>
+                </div>
+                <div className="navLinks">
+                    <Link to={'/Albums-Dashboard/Artists'}>Artists</Link>
+                </div>
+                <div className="navLinks">
+                    <Link to={'/Albums-Dashboard/Tracks'}>Tracks</Link>
+                </div>
             </div>
             <div className='iconContainer'>
                 <Link to={'http://tomas-alberdi.com'} target={'_blank'}><img src="/favicon.ico" alt="Project Icon" /></Link>
