979036b2b8c5b94bf454656a33102a0b785bd9c7
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-04-01 20:09:37 -0300

    Implemented period order by

--- src/components/pages/Albums.jsx
+++ src/components/pages/Albums.jsx
@@ -4,13 +4,13 @@ import Album from '../utils/Album'
 const Albums = ({BASE_URL,API_KEY}) => {
     
     const METHOD = "?method=user.gettopalbums&user=tomyalberdi"
-    const PERIOD = "period=1month"
-
+    const [Period, setPeriod] = useState("1month")
     const [Data, setData] = useState()
     const [LoadingData, setLoadingData] = useState(true)
     useEffect(() => {
         const fetchData = async () => {
-            fetch(`${BASE_URL}/${METHOD}&${API_KEY}&format=json&${PERIOD}&limit=48`)
+            let url = `${BASE_URL}/${METHOD}&${API_KEY}&format=json&period=${Period}&limit=48`
+            fetch(url)
             .then(res => res.json())
             .then(data => {
                 setData(data.topalbums.album)
@@ -18,12 +18,23 @@ const Albums = ({BASE_URL,API_KEY}) => {
             })
         }
         fetchData()
-    }, [LoadingData])
+    }, [LoadingData,Period])
+
+    const changePeriod = () => {
+        setPeriod(document.querySelector("#period").value)
+    }
     
     return (
         <section className='Albums'>
             <section className="filter">
-                <h2>Filter</h2>
+                <select name="period" id="period" defaultValue="1month" onChange={changePeriod}>
+                    <option value="7day">Past 7 days</option>
+                    <option value="1month">Past month</option>
+                    <option value="3month">Past 3 months</option>
+                    <option value="6month">Past 6 months</option>
+                    <option value="12month">Past year</option>
+                    <option value="overall">All Time</option>
+                </select>
             </section>
             <section className="content">
                 {
