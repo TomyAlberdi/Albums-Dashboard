31c717b316663846925f400bb197bea7ecdc0db9
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-03-31 22:30:41 -0300

    Implemented Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className='Footer'>
-            <h2>Footer</h2>
+            <h2>Made with ❤️ in Argentina</h2>
         </footer>
     )
 }
