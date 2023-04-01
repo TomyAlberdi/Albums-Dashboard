51ee0ae943885d7205afdf6257de4a379b0d3ff1
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-04-01 00:53:06 -0300

    Añadida animación de carga

--- src/components/pages/Albums.jsx
+++ src/components/pages/Albums.jsx
@@ -1,13 +1,36 @@
 import React, { useEffect, useState } from 'react'
 
 const Albums = ({BASE_URL,API_KEY}) => {
+    
+    const METHOD = "?method=user.gettopalbums&user=tomyalberdi"
+    const PERIOD = "period=1month"
+
+    const [Data, setData] = useState({})
+    const [LoadingData, setLoadingData] = useState(true)
+    useEffect(() => {
+        const fetchData = async () => {
+            fetch(`${BASE_URL}/${METHOD}&${API_KEY}&format=json&${PERIOD}&limit=20`)
+            .then(res => res.json())
+            .then(data => {
+                setData(data)
+                setLoadingData(false)
+            })
+        }
+        fetchData()
+    }, [LoadingData])
+    
     return (
         <section className='Albums'>
             <section className="filter">
                 <h2>Filter</h2>
             </section>
             <section className="content">
-                <h2>Albums</h2>
+                {
+                    LoadingData ?
+                        <h2 className="loadingMsg">Loading Albums</h2>
+                    :
+                        <h2>Listo</h2>
+                }
             </section>
         </section>
     )
