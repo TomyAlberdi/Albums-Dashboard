28878eded58119669a91ea7602afab9d18094b9d
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-04-05 23:22:51 -0300

    Added 'Go Back' button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
  
 const Navbar = () => {
+
+    const navigate = useNavigate()
 
     const scrollTop = () => {
         window.scrollTo(0,0)
@@ -9,14 +11,25 @@ const Navbar = () => {
 
     return (
         <header className='Navbar'>
-            <section className="goTop" onClick={scrollTop}>
-                <div className="text">
-                    <span>Go</span>
-                    <span>To</span>
-                    <span>Top</span>
-                </div>
-                <i className="fa-solid fa-chevron-up"></i>
-            </section>
+            <div className="func">
+                <section className="goTop" onClick={scrollTop}>
+                    <div className="text">
+                        <span>Go</span>
+                        <span>To</span>
+                        <span>Top</span>
+                    </div>
+                    <i className="fa-solid fa-chevron-up"></i>
+                </section>
+                <section className="goBack" onClick={() => {
+                    navigate(-1)
+                }}>
+                    <div className="text">
+                        <span>Go</span>
+                        <span>Back</span>
+                    </div>
+                    <i className="fa-solid fa-chevron-left"></i>
+                </section>
+            </div>
             <div className='iconContainer'>
                 <Link to={'http://tomas-alberdi.com'} target={'_blank'}><img src="/Albums-Dashboard/favicon.ico" alt="Project Icon" /></Link>
             </div>
@@ -24,4 +37,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar
+export default Navbar;
